3a03d5c0e14e913a6a44c32763a02ef7cf6cb776
Jeu14 <jedissonsantana34@gmail.com>
2024-11-18 16:23:35 -0300

    feat(User): Add register User

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,13 +1,20 @@
 import User from '#models/user'
 import type { HttpContext } from '@adonisjs/core/http'
+import hash from '@adonisjs/core/services/hash'
 
 export default class UsersController {
   public async store({ request, response }: HttpContext) {
     const data = request.only(['email', 'password'])
 
     try {
+      const hashedPassword = await hash.make(data.password)
+      data.password = hashedPassword
+
       const user = await User.create(data)
-      return response.status(201).json(user)
+      return response.status(201).json({
+        id: user.id,
+        email: user.email,
+      })
     } catch (error) {
       return response.status(400).json({ message: 'Erro ao criar o usuário', error: error.message })
     }
