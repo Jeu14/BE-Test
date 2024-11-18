eb1949e331f2726228b8710c06a81101ffb20cda
Jeu14 <jedissonsantana34@gmail.com>
2024-11-18 20:01:32 -0300

    feat(Client): Add client (without auth)

---
+++ app/controllers/clients_controller.ts
@@ -0,0 +1,38 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+import Client from '#models/client'
+import { rules, schema } from '@adonisjs/validator'
+
+export default class ClientsController {
+  public async store({ request, response }: HttpContext) {
+    const validationSchema = schema.create({
+      name: schema.string({}, [rules.required()]),
+      cpf: schema.string({}, [rules.required(), rules.regex(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)]),
+      user_id: schema.number([rules.required()]),
+    })
+
+    const data = await request.validate({ schema: validationSchema })
+
+    const existingClient = await Client.findBy('cpf', data.cpf)
+    if (existingClient) {
+      return response.status(400).json({ message: 'Client CPF already exists' })
+    }
+
+    try {
+      const client = await Client.create(data)
+
+      return response.status(201).json({
+        message: 'Client registered successfully',
+        id: client.id,
+        name: client.name,
+        cpf: client.cpf,
+        user_id: client.userId,
+      })
+    } catch (error) {
+      return response.status(400).json({
+        message: 'Error creating client',
+        error: error.message,
+      })
+    }
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -2,6 +2,9 @@ import router from '@adonisjs/core/services/router'
 
 const AuthController = () => import('#controllers/auth_controller')
 const UsersController = () => import('#controllers/users_controller')
+const ClientsController = () => import('#controllers/clients_controller')
 
 router.post('signup', [UsersController, 'store'])
 router.post('/login', [AuthController, 'login'])
+
+router.post('/store', [ClientsController, 'store'])
